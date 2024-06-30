dcbc945a1af5e7c47479533608479ddfd392762d
Martharuv <mruvalcabaf1900@alumno.ipn.mx>
2024-06-29 20:02:14 -0600

    1 pdfcontroller

--- src/main/java/com/ipn/mx/integration/PdfController.java
+++ src/main/java/com/ipn/mx/integration/PdfController.java
@@ -1,6 +1,10 @@
 package com.ipn.mx.integration;
 
 import com.ipn.mx.service.PdfService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -15,11 +19,17 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/reportes")
+@Api(value = "PdfController", description = "Controlador para la generación de reportes PDF")
 public class PdfController {
 
     @Autowired
     private PdfService pdfService;
 
+    @ApiOperation(value = "Generar reporte PDF de centros de donación", response = InputStreamResource.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Reporte generado exitosamente", response = InputStreamResource.class),
+        @ApiResponse(code = 500, message = "Error al generar el reporte")
+    })
     @GetMapping(value = "/centros/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> generarReporte() throws IOException {
         ByteArrayInputStream bis = pdfService.generarReporte();
